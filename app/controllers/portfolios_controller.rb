60fc6bda277b4d913039845177f3526c23118e91
Jordan Hudgens <jordan@devcamp.com>
2017-06-21 17:27:46 +0000

    Added portfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -5,17 +5,31 @@ class PortfoliosController < ApplicationController
 
       def new
    @portfolio_item= Portfolio.new
-   end
+      end
    
    def create
       @portfolio_item = Portfolio.new (params.require(:portfolio).permit(:title, :subtitle, :body))
       
       respond_to do |format|
-         if @blog.save
+         if @portfolio_item.save
             format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
          else
             format.html { render :new }
          end
       end
    end
- end
+   
+   def edit
+      @portfolio_item = Portfolio.find(params[:id])
+   end
+   def update
+      @portfolio_item = Portfolio.find(params[:id])
+      respond_to do |format|
+         if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+            format.html { redirect_to portfolio_path, notice: 'Portfolio successfully updated'}
+         else
+            format.html { render :edit}
+         end
+      end
+   end
+end
