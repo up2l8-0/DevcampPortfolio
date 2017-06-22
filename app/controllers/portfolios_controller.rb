461800169ffa0bd3a09e26dd49ccf3cb3e6402b6
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 14:44:29 +0000

    Implemented show function

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,8 +2,6 @@ class PortfoliosController < ApplicationController
    def index
       @portfolio_items = Portfolio.all
    end
-   
-   
 
       def new
    @portfolio_item= Portfolio.new
@@ -33,5 +31,9 @@ class PortfoliosController < ApplicationController
             format.html { render :edit}
          end
       end
+   end
+   
+   def show
+      @portfolio_item = Portfolio.find(params[:id])
    end
 end
