6209122adffa8816ac8d0cb7e27d3c04f0b52f80
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 15:07:22 +0000

    Made Destroy function

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -36,4 +36,17 @@ class PortfoliosController < ApplicationController
    def show
       @portfolio_item = Portfolio.find(params[:id])
    end
+   
+   def destroy
+      #perform the lookup
+      @portfolio_item = Portfolio.find(params[:id])
+      
+      #destroy/delete the record
+      @portfolio_item.destroy
+      
+      #redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
+    end
+   end
 end
