27781261bf710f982eeb96d2a312483919121708
Jordan Hudgens <jordan@devcamp.com>
2017-07-11 16:50:20 +0000

    Ward tool initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::Base
    include SetSource
    include CurrentUserConcern
    include DefaultPageContent
+   
+   before_action :set_copyright
+   
+   def set_copyright
+      @copyright = WardViewTool::Renderer.copright 'Mark Ward', 'All rights reserved'
+   end
+end
+
+module WardViewTool
+   class Renderer
+      def self.copright name, msg
+         "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+      end
+   end
 end
